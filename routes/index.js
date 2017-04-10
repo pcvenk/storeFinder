80809adc0f07d6c20f726d098fc0010c7f9b094b
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-04-10 11:32:10 +0200

    Add new store form

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router  = express.Router();
 
+//Show route
 router.get('/', (req, res) => {
    res.render('search-store');
 });
+
+//New store route
+router.get('/addstore', (req, res) => {
+    res.render('add-store');
+});
+
 
 module.exports = router;
